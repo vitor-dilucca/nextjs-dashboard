f296e8e4a0eb2ce76a36ccba15c67ee378a56718
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-28 23:03:11 -0300

    fazendo subroutes e layouts para todos os filhos da rota pai

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout(
+  {children} : {children:React.ReactNode}
+  ){
+  return(
+    <section>
+      Esto es el layout del dashboard
+      {children}
+    </section>
+  )
+}
